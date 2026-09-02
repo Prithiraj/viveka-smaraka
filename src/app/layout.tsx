37cedbb5e38fe54ee23824cc279ceacf7f71c85c
Prithiraj <prithirajsengupta@gmail.com>
2026-09-02 13:24:59 +0530

    style: load mobile experience layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./phase3.css";
 import "./phase4.css";
 import "./phase5.css";
 import "./phase6.css";
+import "./phase7.css";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { JsonLd } from "@/components/seo/JsonLd";
