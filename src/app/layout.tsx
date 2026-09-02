fbf43a9e2ad3b7d42f1e80edbfff4792442facbf
Prithiraj <prithirajsengupta@gmail.com>
2026-09-02 13:16:18 +0530

    style: load portrait hierarchy corrections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./phase2.css";
 import "./phase3.css";
 import "./phase4.css";
 import "./phase5.css";
+import "./phase6.css";
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { SiteHeader } from "@/components/layout/SiteHeader";
 import { JsonLd } from "@/components/seo/JsonLd";
